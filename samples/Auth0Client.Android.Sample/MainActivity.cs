540f4e22bc83997e3ed4af269fab619cb635877f
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-08-01 15:58:31 -0300

    finished adroid sample

--- samples/Auth0Client.Android.Sample/MainActivity.cs
+++ samples/Auth0Client.Android.Sample/MainActivity.cs
@@ -6,50 +6,89 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace Auth0Client.Sample
+namespace Auth0Client.Android.Sample
 {
-	[Activity (Label = "Auth0Client.Sample", MainLauncher = true)]
+	[Activity (Label = "Auth0Client.Android.Sample", MainLauncher = true)]
 	public class Activity1 : Activity
 	{
-		private void wireLogin( Auth0.SDK.Auth0Client client )
-		{
-			client.Completed += (object sender, Xamarin.Auth.AuthenticatorCompletedEventArgs e) => {
-				FindViewById<TextView>(Resource.Id.userName).Text = "User: " + e.Account.Username;
-				FindViewById<TextView>(Resource.Id.id_token).Text = "Id Token: " + (string)e.Account.Properties["id_token"];
-			};
-
-			client.Error += (object sender, Xamarin.Auth.AuthenticatorErrorEventArgs e) => {
-				FindViewById<TextView>(Resource.Id.userName).Text = "Error: " + e.Message;
-			};
-
-			var intent = client.GetUI (this);
-			StartActivityForResult(intent, 42);
-		}
+		// You can obtain {tenant} and {clientID} from your settings page in the Auth0 Dashboard (https://app.auth0.com/#/settings)
+		private const string Tenant = "iaco2";
+		private const string ClientID = "XviE9dLlREjXZduIzTqtsGsiZELjls8z";
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
+			SetContentView(Resource.Layout.Main);
+
+			FindViewById<TextView>(Resource.Id.id_token).Text = string.Empty;
+			FindViewById<TextView>(Resource.Id.userName).Text = string.Empty;
+
 			Button loginWidget = FindViewById<Button> (Resource.Id.loginWidget);
 
 			loginWidget.Click += delegate {
+				// This will show all connections enabled in Auth0, and let the user choose the identity provider
+				var client = new Auth0.SDK.Auth0Client (
+					"Auth0", 						// title
+					Tenant, 						// tenant
+					ClientID, 						// clientID
+					"https://localhost/client");	// callback
 
-				//This will show all connections enabled in Auth0, and let the user choose the identity provider
-				var auth0 = new Auth0.SDK.Auth0Client( "Auth0", "eugeniop", "2iuy7pGx4Q1ITmUwCvgziYtdkclejcqt", "http://localhost/client");
-				wireLogin( auth0 );
+				this.wireLogin(client);
 			};
-
 
 			Button loginConnection = FindViewById<Button> (Resource.Id.loginConnection);
 
 			loginConnection.Click += delegate {
+				// This uses a specific connection: google-oauth2
+				var client = new Auth0.SDK.Auth0Client (
+					"Auth0", 						// title
+					Tenant, 						// tenant
+					ClientID, 						// clientID
+					"google-oauth2",				// connection name
+					"https://localhost/client");	// callback
 
-				//This uses a specific connection: google-ouath2
-				var auth0 = new Auth0.SDK.Auth0Client( "Auth0", "eugeniop", "2iuy7pGx4Q1ITmUwCvgziYtdkclejcqt", "google-oauth2", "http://localhost/client");
-				wireLogin (auth0);
+				this.wireLogin(client);
 			};
+		}
+
+		private void wireLogin (Auth0.SDK.Auth0Client client)
+		{
+			client.Completed += (object sender, Xamarin.Auth.AuthenticatorCompletedEventArgs e) => 
+			{
+				if (e.IsAuthenticated) 
+				{
+					// All the information gathered from a successful authentication is available in e.Account
+					this.ShowResult(
+						idToken: (string)e.Account.Properties["id_token"],
+						userName: e.Account.Username);
+				}
+				else
+				{
+					// The user cancelled
+					this.ShowResult(error: "User cancelled");
+				}
+			};
+
+			client.Error += (object sender, Xamarin.Auth.AuthenticatorErrorEventArgs e) =>
+			{
+				this.ShowResult(error: e.Message);
+			};
+
+			// We're ready to present the login UI
+			var intent = client.GetUI (this);
+			StartActivityForResult(intent, 42);
+		}
+
+		private void ShowResult(string idToken = "", string userName = "", string error = "")
+		{
+			FindViewById<TextView>(Resource.Id.id_token).Text = 
+				!string.IsNullOrEmpty(idToken) ? 
+					string.Format("id_token: {0}", idToken) : string.Empty;
+
+			FindViewById<TextView>(Resource.Id.userName).Text = 
+				string.IsNullOrEmpty(error) ? 
+					string.Format("Hi {0}!", userName) : "Error: " + error;
 		}
 	}
 }
-
-
--- samples/Auth0Client.Android.Sample/Properties/AssemblyInfo.cs
+++ samples/Auth0Client.Android.Sample/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Auth0Client.Sample")]
+[assembly: AssemblyTitle("Auth0Client.Android.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
