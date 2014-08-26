b83cf799614cd12b057551aea7b27062a3b10fc9
dschenkelman <damian.schenkelman@gmail.com>
2014-08-26 15:34:58 -0300

    Updating impl of device id provider based on feedback

--- Auth0Client.Android/DeviceIdProvider.cs
+++ Auth0Client.Android/DeviceIdProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Android.Provider;
 using System.Threading.Tasks;
-using Android.Accounts;
 using Android.OS;
 
 namespace Auth0.SDK
@@ -10,7 +9,7 @@ namespace Auth0.SDK
 	{
 		public Task<string> GetDeviceId()
 		{
-			return Task.FromResult<string>(Build.Fingerprint);
+			return Task.FromResult<string>(string.Format("{0} {1}", Build.Brand, Build.Model));
 		}
 	}
 }
