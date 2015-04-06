b2f6a6b4ce64d660d0c3ee7e184092b9a70dd763
Hernan Zalazar <hernanzalazar@gmail.com>
2015-04-06 11:54:36 -0300

    Update samples.

--- samples/Auth0Client.Android.Sample/Resources/Resource.designer.cs
+++ samples/Auth0Client.Android.Sample/Resources/Resource.designer.cs
@@ -48,9 +48,6 @@ namespace Auth0Client.Android.Sample
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
-			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
-			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -64,32 +61,32 @@ namespace Auth0Client.Android.Sample
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060002
-			public const int loginWithConnection = 2131099650;
+			// aapt resource value: 0x7f050002
+			public const int loginWithConnection = 2131034114;
 			
-			// aapt resource value: 0x7f060005
-			public const int loginWithUserPassword = 2131099653;
+			// aapt resource value: 0x7f050005
+			public const int loginWithUserPassword = 2131034117;
 			
-			// aapt resource value: 0x7f060000
-			public const int loginWithWidget = 2131099648;
+			// aapt resource value: 0x7f050000
+			public const int loginWithWidget = 2131034112;
 			
-			// aapt resource value: 0x7f060001
-			public const int loginWithWidgetAndRefreshToken = 2131099649;
+			// aapt resource value: 0x7f050001
+			public const int loginWithWidgetAndRefreshToken = 2131034113;
 			
-			// aapt resource value: 0x7f060006
-			public const int refreshWithIdToken = 2131099654;
+			// aapt resource value: 0x7f050006
+			public const int refreshWithIdToken = 2131034118;
 			
-			// aapt resource value: 0x7f060007
-			public const int refreshWithRefreshToken = 2131099655;
+			// aapt resource value: 0x7f050007
+			public const int refreshWithRefreshToken = 2131034119;
 			
-			// aapt resource value: 0x7f060008
-			public const int txtResult = 2131099656;
+			// aapt resource value: 0x7f050008
+			public const int txtResult = 2131034120;
 			
-			// aapt resource value: 0x7f060003
-			public const int txtUserName = 2131099651;
+			// aapt resource value: 0x7f050003
+			public const int txtUserName = 2131034115;
 			
-			// aapt resource value: 0x7f060004
-			public const int txtUserPassword = 2131099652;
+			// aapt resource value: 0x7f050004
+			public const int txtUserPassword = 2131034116;
 			
 			static Id()
 			{
@@ -132,22 +129,6 @@ namespace Auth0Client.Android.Sample
 			}
 			
 			private String()
-			{
-			}
-		}
-		
-		public partial class Style
-		{
-			
-			// aapt resource value: 0x7f050000
-			public const int Mono_Android_Theme_Splash = 2131034112;
-			
-			static Style()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Style()
 			{
 			}
 		}
